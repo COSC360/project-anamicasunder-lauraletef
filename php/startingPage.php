cc0addc9196a64e2f59bef364f5ebe26cd6d37ca
Anamica S <71396652+anamicasunder@users.noreply.github.com>
2023-03-22 16:49:23 -0700

    changes of style

--- php/startingPage.php
+++ php/startingPage.php
@@ -1,9 +1,45 @@
 <!DOCTYPE html>
 <html>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="html/css/main.css">
+    
+    <style>
+        body{
+    background-color: #FFFCF7;
+
+}
+
+h1{
+    font-family:Georgia, 'Times New Roman', Times, serif;
+    font-size: 500%;
+    margin-left: 30%;
+    margin-top:10%;
+    color: #738290;
+
+}
+
+.login{
+    background-color: #E4F0D0;
+    size: 300%;
+}
+
+.signup{
+    background-color: #C2D8B9;
+  
+}
+
+button{
+    display: flex;
+    margin-left: 45%;
+}
+
+
+    </style>
+
+
+    
 
    <head>
-     <h1>blogalert!</h1> 
+     <h1>blogalert! </h1> 
    </head>
    <body>
       <button type = "button" class = "login">log in</button> 
