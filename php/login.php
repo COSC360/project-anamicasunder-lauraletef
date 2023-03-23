322e7c32672ef6bea28b35340717e37d293c0795
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-22 23:09:33 -0700

    signup done

--- php/login.php
+++ php/login.php
@@ -1,6 +1,79 @@
 <!DOCTYPE html>
 <html>
+<head>
+     <h1>blogalert!</h1> 
+   </head>
+<style>
 
+body{
+    background-color: #FFFCF7;
+
+}
+form input[type="text"] {
+  display: block;
+ 
+}
+
+h1{
+    font-family:Georgia, 'Times New Roman', Times, serif;
+        font-size: 500%;
+        color: #738290;
+        display: flex;
+        margin-top: 2.5em;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 0.3em;
+        padding-bottom: 0.1em;
+}
+
+form{
+    background-color: #E4F0D0;
+        font-family:Georgia, 'Times New Roman', Times, serif;
+        color: #515c66;
+        font-weight: bold;
+        justify-content: center;
+        text-align: center;
+        display: block; 
+        margin-left: 30%;
+        margin-right: 30%;
+        padding:2em;
+        border-radius: 30px;
+}
+input{
+    font-family:Georgia;
+    background-color:  #FFFCF7;
+    color: #515c66;
+    text-align: center;
+    width: 100%;
+    border-radius: 40px;
+    box-sizing: border-box;
+    font-size: 130%;
+    display: block;
+    padding:20px;
+}
+
+.submit{
+    display: block;
+        justify-content: space-evenly;
+        width: 400px;
+        padding: 1em;
+        height: 70px;
+        margin: auto;
+        border-radius: 30px;
+        border-width: 0em;
+        font-size: 145%;
+        transition-duration: 0.4s;
+        padding-left:30px;
+    padding-right: 30px;
+      
+    } 
+    .submit:hover{
+        background-color: #767d6b;
+        color: #FFFCF7;
+        cursor: pointer ;
+    }
+}
+</style>
 <body>
 
 <?php
@@ -51,15 +124,15 @@ if ($stmt->execute()) {
 
 
 <form method="POST" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-   <label for= "username">UserName:</label><br>
-
-   <input type="text" name="login" required><br><br>
    
-   <label for="password">Password</label> <br>
-   <input type="text" name="password" required><br>
+
+   <input type="text" class="username" name="login" placeholder="username"required><br><br>
+   
+ 
+   <input type="text" class="password" name="password" placeholder = "password"required><br>
    <br>
 <br>
-   <input type="submit" value="Submit">
+   <input type="submit" class="submit" value="log in!">
 </form>
 
 
