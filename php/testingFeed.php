80a307baf7e1b4749537e32974468619aea706fd
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-26 14:17:43 -0700

    testing blog display

--- php/testingFeed.php
+++ php/testingFeed.php
@@ -13,5 +13,18 @@ if (isset($_SESSION['username'])) {
     header("Location: testingLogin.php");
     exit();
 }
+
+
+// Query the database to get the blog posts
+$sql = "SELECT * FROM blogpost ORDER BY date_posted DESC";
+$result = $conn->query($sql);
+
+// Loop through the result and display the blog posts
+while ($row = $result->fetch_assoc()) {
+    echo "<h2>" . $row["username"] . "</h2>";
+    echo "<p>" . $row["post"] . "</p>";
+    echo "<p>Posted on " . $row["date_posted"] . "</p>";
+}
+
 ?>
 </html>
