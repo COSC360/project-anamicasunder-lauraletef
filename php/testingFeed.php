bb8795ea2f4161c822f9b1c20c1b13668359186e
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-26 14:09:32 -0700

    test

--- php/testingFeed.php
+++ php/testingFeed.php
@@ -2,6 +2,16 @@
 <html>
 <p> hello world!</p>
 <?php
-    echo "$username";
-    ?>
+session_start();
+
+// Check if the user is logged in
+if (isset($_SESSION['username'])) {
+    // User is logged in, display their name
+    echo "Welcome, " . $_SESSION['username'] . "!";
+} else {
+    // User is not logged in, redirect to the login page
+    header("Location: testingLogin.php");
+    exit();
+}
+?>
 </html>
