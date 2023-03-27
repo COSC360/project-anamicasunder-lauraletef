e4cc22243f16268f226ea80b4c6e706af86d31c9
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-27 00:11:31 -0700

    reverting mistakes

--- php/testingLogin.php
+++ php/testingLogin.php
@@ -2,32 +2,28 @@
      <h1>blogalert!</h1> 
    </head>
 <?php
-
 session_start();
-
 // Set up MySQL database connection
 $servername = "localhost";
 $username = "24466963";
 $password = "24466963";
 $dbname = "db_24466963";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check if there was an error connecting to the database
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Check if the user is logged in
-if (isset($_SESSION['username'])) {
-    echo "Welcome, " . $_SESSION['username'] . "! You are already logged in.";
-        //  if(isset($_SESSION['username']) == 'coscadmin'){
-        //      boolean admin = true;
-        //     }
+ // Check if the user is logged in
+ if (isset($_SESSION['username'])) {
+     echo "Welcome, " . $_SESSION['username'] . "! You are already logged in.";
+          if(isset($_SESSION['username']) == 'coscadmin'){
+              boolean admin = true;
+             }
            
 
-            
-    
+
+
+
     echo '<button type="button" onclick="window.location.href=\'blogtest.php\'">Continue to Feed</button>';
     if (isset($_SESSION['username'])) {
         // User is logged in, display logout button
@@ -40,19 +36,16 @@ if (isset($_SESSION['username'])) {
       header("Location: startingPage.php"); // redirect to login page
       exit;
     }
-}
- else {
+} else {
     // User is not logged in, show login form
     // Check if the user submitted the login form
     if (isset($_POST['login'])) {
         // Get the submitted username and password
         $username = $_POST['username'];
         $password = $_POST['password'];
-
         // Query the database to check if the user exists
         $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $result = $conn->query($sql);
-
         if ($result->num_rows > 0) {
             // User exists, log them in
             session_start();
@@ -66,40 +59,34 @@ if (isset($_SESSION['username'])) {
     }
 }
 ?>
-
 <!-- HTML form for user to log in -->
 <form method="post" action="">
     <label>Username:</label>
     <input type="text" name="username"><br>
-
     <label>Password:</label>
     <input type="password" name="password"><br>
-
     <input type="submit" name="login" value="Log In">
     
     <button type="button" class="create" onclick="window.location.href='testingSignup.php'">Create Account</button>
     <br>
 </form>
-
 <?php
     // Display error message if there was a login error
-    if (isset($error)) {
-        echo "<p>$error</p>";
-    }
+     if (isset($error)) {
+         echo "<p>$error</p>";
+     }
+ }
 
-?>
+ ?>
 
-<style>
+ <style>
     body{
     background-color: #FFFCF7;
-
 }
 form input[type="text"] {
   display: block;
  
 }
-
-
 h1{
     font-family:Georgia, 'Times New Roman', Times, serif;
         font-size: 500%;
@@ -113,8 +100,6 @@ h1{
         padding-bottom: 0.1em;
  
 }
-
-
 form{
     background-color: #E4F0D0;
         font-family:Georgia, 'Times New Roman', Times, serif;
@@ -141,7 +126,6 @@ input{
     padding:20px;
     border: 0em;
 }
-
 .submit{
     display: block;
     text-align: center;
@@ -159,7 +143,4 @@ input{
         color: #FFFCF7;
         cursor: pointer ;
     }
-
-
     </style>
-
