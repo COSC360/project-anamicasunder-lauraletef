06f146ead5c762420a268158e883b8f1c29b4c63
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-26 19:51:17 -0700

    undo that

--- php/blogtest.php
+++ php/blogtest.php
@@ -153,65 +153,6 @@ if (isset($_SESSION['username'])) {
    }
 
 
-
-
-
-   if (isset($_POST['submit'])) {
-    // Get the submitted post data
-    $username = $_SESSION['username'];
-    $content = $_POST['content'];
-    $post_id = $SESSION['post_id']; 
-
-
-    // Insert the new post into the database
-    $sql = "INSERT INTO comments (username, content, post_id) VALUES ('$username', '$content', '$post_id')";
-    if ($conn->query($sql) === TRUE) {
-        echo "<p>Post added successfully.</p>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-
-// Display the form to add a new comment
-echo '<form method="post" action="">
-        <label for="comment">Comment:</label>
-        <textarea name="comment" id="comment"></textarea><br><br>
-        <input type="submit" name="submit" value="Add Comment">
-      </form>';
-
-
- else {}
-// User is not logged in, display comments based on keyword search
-if (isset($_POST['search'])) {
-    $search_query = $_POST['search'];
-    $sql = "SELECT DISTINCT * FROM commentas WHERE content LIKE '%$search_query%'";
-    $result = $conn->query($sql);
-} else {
-    $sql = "SELECT DISTINCT * FROM comments";
-    $result = $conn->query($sql);
-}
-
-
-if ($result->num_rows > 0) {
-    // Display all the comments in the database
-    echo '<form method="post" action="">
-            <label for="search">Search by username or keyword:</label>
-            <input type="text" name="search" id="search">
-            <input type="submit" value="Search">
-            <input type="button" value="Clear" onclick="window.location=\'\'">
-          </form><br>';
-    while ($row = $result->fetch_assoc()) {
-        echo "<h2>" . $row['username'] . "</h2>";
-        echo "<p>" . $row['content'] . "</p>";
-        echo "<hr>";
-    }
-} else {
-    echo "No posts found.";
-}
-
-
-
 // Close the database connection
 $conn->close();
 ?>
