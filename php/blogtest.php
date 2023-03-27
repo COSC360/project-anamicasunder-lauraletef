7ae751639277255880c07af3713bcb218124f540
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-27 00:21:55 -0700

    changes

--- php/blogtest.php
+++ php/blogtest.php
@@ -8,24 +8,18 @@
     </nav>
 
 
-<?php
+    <?php
 session_start();
-
-
 // Connect to the MySQL database
 $servername = "localhost";
 $username = "24466963";
 $password = "24466963";
 $dbname = "db_24466963";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-
 // Check if there was an error connecting to the database
 if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
-
-
 // Check if the user is logged in
 if (isset($_SESSION['username'])) {
    // User is logged in, display their name and logout button
@@ -33,8 +27,6 @@ if (isset($_SESSION['username'])) {
    echo '<form method="post" action="">
            <input type="submit" name="logout" value="Logout">
          </form>';
-
-
    // Check if the form to add a new post was submitted
    if (isset($_POST['submit_post'])) {
        // Get the submitted post data
@@ -42,18 +34,15 @@ if (isset($_SESSION['username'])) {
        $post = $_POST['post'];
        $date_posted = date('Y-m-d H:i:s');
        $post_id = uniqid(); // Generate random post ID
-
-
-       // Insert the new post into the database
-       $sql = "INSERT INTO blogpost (post_id, username, post, date_posted) VALUES ('$post_id', '$username', '$post', '$date_posted')";
-       if ($conn->query($sql) === TRUE) {
+        // Insert the new post into the database
+        $sql = "INSERT INTO blogpost (post_id, username, post, date_posted) VALUES ('$post_id', '$username', '$post', '$date_posted')";
+        if ($conn->query($sql) === TRUE) {
            echo "<p>Post added successfully.</p>";
-       } else {
-           echo "Error: " . $sql . "<br>" . $conn->error;
-       }
+            echo "<p>Post added successfully.</p>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
    }
-
-
    // Handle the submission of a new comment
 if (isset($_POST['submit_comment'])) {
    // Get the submitted comment data
@@ -61,29 +50,20 @@ if (isset($_POST['submit_comment'])) {
    $post_id = $_POST['post_id'];
    $comment = $_POST['comment'];
    $date_posted = date('Y-m-d H:i:s');
-
-
-// Insert the new comment into the database
-        $sql = "INSERT INTO comments (post_id, username, comment, date_posted) VALUES ('$post_id', '$username', '$comment', '$date_posted')";
-        if ($conn->query($sql) === TRUE) {
-            echo "<p>Comment added successfully.</p>";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
-
-
-
-
-
-
+ // Insert the new comment into the database
+         $sql = "INSERT INTO comments (post_id, username, comment, date_posted) VALUES ('$post_id', '$username', '$comment', '$date_posted')";
+         if ($conn->query($sql) === TRUE) {
+           echo "<p>Comment added successfully.</p>";
+             echo "<p>Comment added successfully.</p>";
+         } else {
+             echo "Error: " . $sql . "<br>" . $conn->error;
+         }
 }
   // Check if the comments for this post have already been retrieved
         if (!isset($_SESSION['comments'][$post_id])) {
             // Comments have not been retrieved yet, make a new request
             $sql_comments = "SELECT DISTINCT * FROM comments WHERE post_id = '$post_id' ORDER BY date_posted ASC";
             $result_comments = $conn->query($sql_comments);
-
-
             if ($result_comments->num_rows > 0) {
                 // Save the retrieved comments in the session variable
                 $comments = array();
@@ -96,21 +76,13 @@ if (isset($_POST['submit_comment'])) {
                 $_SESSION['comments'][$post_id] = array();
             }
         }
-
-
             }
-
-
-
-
    // Display the form to add a new post
    echo '<form method="post" action="">
            <label for="post">Post:</label>
            <textarea name="post" id="post"></textarea><br><br>
            <input type="submit" name="submit_post" value="Add Post">
          </form>';
-
-
    // Display all the posts in the database
    $sql = "SELECT DISTINCT * FROM blogpost ORDER BY date_posted DESC";
    $result = $conn->query($sql);
@@ -129,8 +101,6 @@ if (isset($_POST['submit_comment'])) {
    $post_id = $row['post_id'];
    $sql_comments = "SELECT DISTINCT * FROM comments WHERE post_id = '$post_id' ORDER BY date_posted ASC";
    $result_comments = $conn->query($sql_comments);
-
-
    if ($result_comments->num_rows > 0) {
        // Display all the comments for this post
        echo "<h3>Comments:</h3>";
@@ -141,16 +111,12 @@ if (isset($_POST['submit_comment'])) {
    } else {
        echo "<p>No comments yet.</p>";
    }
-
-
    echo "<hr>";
 }} else {
    echo "No posts found.";
    }
   
  
-
-
 // Close the database connection
 $conn->close();
 ?>
--- php/testingLogin.php
+++ php/testingLogin.php
@@ -1,6 +1,3 @@
-<head>
-     <h1>blogalert!</h1> 
-   </head>
 <?php
 session_start();
 // Set up MySQL database connection
@@ -13,32 +10,25 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
- // Check if the user is logged in
- if (isset($_SESSION['username'])) {
+// Check if the user is logged in
+if (isset($_SESSION['username'])) {
      echo "Welcome, " . $_SESSION['username'] . "! You are already logged in.";
-          if(isset($_SESSION['username']) == 'coscadmin'){
-              boolean admin = true;
-             }
-           
 
-
-
-
-    echo '<button type="button" onclick="window.location.href=\'blogtest.php\'">Continue to Feed</button>';
-    if (isset($_SESSION['username'])) {
-        // User is logged in, display logout button
-        echo '<form method="post" action="">
-              <input type="submit" name="logout" value="Logout">
-              </form>';
-    } 
-    if (isset($_POST['logout'])) {
-      session_destroy(); // destroy all session data
-      header("Location: startingPage.php"); // redirect to login page
-      exit;
-    }
-} else {
-    // User is not logged in, show login form
-    // Check if the user submitted the login form
+     echo '<button type="button" onclick="window.location.href=\'blogtest.php\'">Continue to Feed</button>';
+     if (isset($_SESSION['username'])) {
+         // User is logged in, display logout button
+         echo '<form method="post" action="">
+               <input type="submit" name="logout" value="Logout">
+               </form>';
+     } 
+     if (isset($_POST['logout'])) {
+       session_destroy(); // destroy all session data
+       header("Location: startingPage.php"); // redirect to login page
+       exit;
+     }
+ } else {
+     // User is not logged in, show login form
+     // Check if the user submitted the login form
     if (isset($_POST['login'])) {
         // Get the submitted username and password
         $username = $_POST['username'];
@@ -57,7 +47,6 @@ if ($conn->connect_error) {
             $error = "Invalid login credentials. Please try again.";
         }
     }
-}
 ?>
 <!-- HTML form for user to log in -->
 <form method="post" action="">
@@ -72,75 +61,8 @@ if ($conn->connect_error) {
 </form>
 <?php
     // Display error message if there was a login error
-     if (isset($error)) {
-         echo "<p>$error</p>";
-     }
- }
-
- ?>
-
- <style>
-    body{
-    background-color: #FFFCF7;
-}
-form input[type="text"] {
-  display: block;
- 
-}
-h1{
-    font-family:Georgia, 'Times New Roman', Times, serif;
-        font-size: 500%;
-        color: #738290;
-        display: flex;
-        position: center;
-        margin-top:1.5em;
-        justify-content: center;
-        align-items: center;
-        margin-bottom: 0.3em;
-        padding-bottom: 0.1em;
- 
-}
-form{
-    background-color: #E4F0D0;
-        font-family:Georgia, 'Times New Roman', Times, serif;
-        color: #515c66;
-        font-weight: bold;
-        justify-content: center;
-        text-align: center;
-        display: block; 
-        margin-left: 30%;
-        margin-right: 30%;
-        padding:2em;
-        border-radius: 30px;
-}
-input{
-    font-family:Georgia;
-    background-color:  #FFFCF7;
-    color: #515c66;
-    text-align: center;
-    width: 100%;
-    border-radius: 40px;
-    box-sizing: border-box;
-    font-size: 130%;
-    display: block;
-    padding:20px;
-    border: 0em;
-}
-.submit{
-    display: block;
-    text-align: center;
-    box-sizing: border-box;
-        width: 100%;
-        border-radius: 40px;
-        padding: 20px;
-        border-radius: 40px;
-        font-size: 145%; 
-        transition-duration: 0.4s;
-      
-    } 
-    .submit:hover{
-        background-color: #767d6b;
-        color: #FFFCF7;
-        cursor: pointer ;
+    if (isset($error)) {
+        echo "<p>$error</p>";
     }
-    </style>
+}
+?>
