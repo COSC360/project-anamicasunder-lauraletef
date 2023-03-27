7195b1632c8ecff192b970827cb2a873804d1964
lletef16 <77820934+lletef16@users.noreply.github.com>
2023-03-26 23:12:18 -0700

    YUP

--- php/admin.php
+++ php/admin.php
@@ -1,4 +1,13 @@
+
 <?php
+// Start the session
+session_start();
+
+// Check if the user is an admin
+if(!isset($_SESSION['username']) || $_SESSION['isAdmin'] != 1) {
+    header("Location: blogtest.php");
+    exit();
+}
 
 // Connect to the database
 $servername = "localhost";
@@ -13,12 +22,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if the user is an admin
-session_start();
-if($row["isAdmin"] != 1) {
-   echo "You're not an admin lol";
-
-
 // Handle deleting a blog post
 if(isset($_POST['delete_post'])) {
     $post_id = $_POST['post_id'];
@@ -27,17 +30,6 @@ if(isset($_POST['delete_post'])) {
         echo "Blog post deleted successfully";
     } else {
         echo "Error deleting blog post: " . $conn->error;
-    }
-}
-
-// Handle deleting a user
-if(isset($_POST['delete_user'])) {
-    $username = $_POST['username'];
-    $sql = "DELETE FROM users WHERE username='$username'";
-    if ($conn->query($sql) === TRUE) {
-        echo "User deleted successfully";
-    } else {
-        echo "Error deleting user: " . $conn->error;
     }
 }
 
@@ -64,33 +56,5 @@ if ($result->num_rows > 0) {
     echo "No blog posts";
 }
 
-// Display all users
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Username: " . $row["username"] . "<br>";
-        echo "First Name: " . $row["firstName"] . "<br>";
-        echo "Last Name: " . $row["lastName"] . "<br>";
-        echo "Email: " . $row["email"] . "<br>";
-        echo "Profile Image: " . $row["profileImage"] . "<br>";
-        echo "Is Admin: " . $row["isAdmin"] . "<br>";
-        echo '<form method="post" action=""><input type="hidden" name="username" value="' . $row["username"] . '">';
-
-        // Only show delete button if the user is an admin and the user being deleted is not an admin
-        if ($_SESSION['isAdmin'] == 1 && $row["isAdmin"] != 1) {
-            echo '<input type="submit" name="delete_user" value="Delete"></form><br><br>';
-} else {
-echo '</form><br><br>';
-}
-}
-} else {
-echo "No users";
-}
-}
-// Close the database connection
 $conn->close();
 ?>
-
